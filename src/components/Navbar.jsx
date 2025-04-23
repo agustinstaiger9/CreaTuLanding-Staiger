e0d12130970adc73923303d1e898d104ee174a23
Agustin Staiger <staiger.agustin9@gmail.com>
2025-04-23 09:00:13 -0300

    modificacion en el carrito

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,24 @@
 import { Link } from "react-router-dom";
-import "../app.css";
+import "../app.css"; // Importa app.css para aplicar los estilos globales
 
 function Navbar() {
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Inicio</Link>
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
+          <Link to="/" className="navbar-link">Inicio</Link>
         </li>
-        <li>
-          <Link to="/category/smartphones">Smartphones</Link>
+        <li className="navbar-item">
+          <Link to="/category/smartphones" className="navbar-link">Smartphones</Link>
         </li>
-        <li>
-          <Link to="/category/laptops">Laptops</Link>
+        <li className="navbar-item">
+          <Link to="/category/laptops" className="navbar-link">Laptops</Link>
         </li>
-        <li>
-          <Link to="/cart">Carrito</Link> {/* Enlace al carrito */}
+        <li className="navbar-item">
+          <Link to="/cart" className="navbar-link cart-icon">
+            <i className="fas fa-shopping-cart"></i> {/* Ícono del carrito */}
+            <span className="cart-bubble">3</span> {/* Burbuja del carrito */}
+          </Link>
         </li>
       </ul>
     </nav>
