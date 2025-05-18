2648dabef425152aa40237669ca60c08bab12f57
Agustin Staiger <staiger.agustin9@gmail.com>
2025-05-18 02:24:29 -0300

    ultimas modificaciones en styles y checkout css

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CartProvider } from "./context/CartContext"; // Importamos el proveedor del carrito
+import { CartProvider } from "./context/CartContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <CartProvider> {/* Aquí envolvemos la app con el CartProvider */}
+    <CartProvider> {}
       <App />
     </CartProvider>
   </React.StrictMode>
